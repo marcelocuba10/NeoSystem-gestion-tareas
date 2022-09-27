d553f860462d207b70a2e3768d4cd1a22f3bc3fa
Marcelo Cuba <marcelo.cuba.10@hotmail.com>
2022-09-27 08:29:21 -0400

    FIX close icon modal gallery product module admin

--- Modules/Admin/Resources/views/products/image-gallery.blade.php
+++ Modules/Admin/Resources/views/products/image-gallery.blade.php
@@ -64,7 +64,7 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title font-weight-bold">Upload Image</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <button type="button" class="btn-close-img" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
